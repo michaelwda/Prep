2dca5cafa8233532f1e7920efc1fc7e1b341c2db
Michael Davis <michael.davis@ptccinc.com>
2020-08-11 22:08:59 -0400

    Init

---
+++ Prep.Problems/Problems/rotting_oranges/Solution.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//https://leetcode.com/problems/rotting-oranges/
+namespace Prep.Problems.Problems.rotting_oranges
+{
+    public class Solution
+    {
+        public int OrangesRotting(int[][] grid)
+        {
+            //Early Exit Conditions:
+            //Empty Grid
+            //Non-Rotten Fruit exists that cannot be reached
+
+            //Empty Grid passed in
+            if (grid.Length == 0)
+                return -1;
+            if (grid[0].Length == 0)
+                return -1;
+
+            var rottenOranges=new Queue<(int, int)>();
+            var freshOrangesLeft = 0;
+            for (var row = 0; row < grid.Length; row++)
+            {
+                for (var column = 0; column < grid[0].Length; column++)
+                {
+                    var orange = GetOrange(grid, row, column);
+                    
+                    //Just to prevent duplicate work, I'm also building a list of rotten and fresh fruit in this step
+                    if (orange == 1)
+                    {
+                        freshOrangesLeft++;
+                    }
+                    else if (orange == 2)
+                    {
+                        rottenOranges.Enqueue((row, column));
+                    }
+
+                }
+            }
+
+            //Using -1 as a tombstone indicating that we've reached the end of a level
+            rottenOranges.Enqueue((-1,-1));
+
+            //BFS
+            int minutes = -1;
+            while (rottenOranges.Any())
+            {
+                var rottenOrange = rottenOranges.Dequeue();
+                //We've hit a tombstone
+                if (rottenOrange.Item1 == -1)
+                {
+                    minutes++;
+                    //We have another level of items to process, add another tombstone
+                    if (rottenOranges.Any())
+                    {
+                        rottenOranges.Enqueue((-1, -1));
+                    }
+                }
+                else
+                {
+                    //We should use a for loop here instead of repeating code...
+                    //Rot neighbors
+          
+                    if (GetOrange(grid, rottenOrange.Item1 - 1, rottenOrange.Item2) == 1)
+                    {
+                        grid[rottenOrange.Item1 - 1][rottenOrange.Item2] = 2;
+                        freshOrangesLeft--;
+                        rottenOranges.Enqueue((rottenOrange.Item1 - 1, rottenOrange.Item2));
+                    }
+                    if (GetOrange(grid, rottenOrange.Item1 + 1, rottenOrange.Item2) == 1)
+                    {
+                        grid[rottenOrange.Item1 + 1][rottenOrange.Item2] = 2;
+                        freshOrangesLeft--;
+                        rottenOranges.Enqueue((rottenOrange.Item1 + 1, rottenOrange.Item2));
+                    }
+                    if (GetOrange(grid, rottenOrange.Item1, rottenOrange.Item2 + 1) == 1)
+                    {
+                        grid[rottenOrange.Item1][rottenOrange.Item2 + 1] = 2;
+                        freshOrangesLeft--;
+                        rottenOranges.Enqueue((rottenOrange.Item1, rottenOrange.Item2 + 1));
+                    }
+                    if (GetOrange(grid, rottenOrange.Item1, rottenOrange.Item2 - 1) == 1)
+                    {
+                        grid[rottenOrange.Item1][rottenOrange.Item2 - 1] = 2;
+                        freshOrangesLeft--;
+                        rottenOranges.Enqueue((rottenOrange.Item1, rottenOrange.Item2 - 1));
+                    }
+                }
+            }
+
+            if (freshOrangesLeft == 0)
+            {
+                return minutes;
+            }
+            else
+            {
+                return -1;
+            }
+             
+
+        }
+
+        public int GetOrange(int[][] grid, int row, int column)
+        {
+            if (grid.Length == 0)
+                return 0;
+            if (row < 0 || column < 0)
+                return 0;
+            if (row >= grid.Length)
+                return 0;
+            if (column >= grid[0].Length)
+                return 0;
+            return grid[row][column];
+        }
+    }
+
+      
+}
---
+++ Prep.Tests/RottingOranges.cs
@@ -0,0 +1,55 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prep.Problems.Problems.rotting_oranges;
+
+namespace Prep.Tests
+{
+    [TestClass]
+    public class RottingOranges
+    {
+        private readonly Solution _solution = new Solution();
+        
+        [TestMethod]
+        public void Example1()
+        {
+            var result = _solution.OrangesRotting(new[]
+            {
+                new[] {2, 1, 1},
+                new[] {1, 1, 0},
+                new[] {0, 1, 1},
+            });
+
+            Assert.AreEqual(4, result);
+        }
+        [TestMethod]
+        public void Example2()
+        {
+            var result = _solution.OrangesRotting(new[]
+            {
+                new[] {2, 1, 1},
+                new[] {0, 1, 1},
+                new[] {1, 0, 1},
+            });
+            Assert.AreEqual(-1, result);
+        }
+        [TestMethod]
+        public void Example3()
+        {
+            var result = _solution.OrangesRotting(new[]
+            {
+                new[] {0,2},
+            });
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void ImpossibleArrangement()
+        {
+            var result = _solution.OrangesRotting(new[]
+                {
+                new[] {2,0,0},
+                new[] {0,0,0},
+                new[] {0,0,1},
+            });
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
