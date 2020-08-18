93dd19d9bf974e7298c0468c0b86e216dba072ff
Michael Davis <michael.davis@ptccinc.com>
2020-08-17 23:42:22 -0400

    shopping cart example problem

---
+++ Prep.Problems/Problems/AWS/ShoppingCart.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prep.Problems.Problems.AWS
+{
+    public class ShoppingCart
+    {
+        public int Solve(List<List<string>> codeList, List<string> shoppingCart)
+        {
+            if (codeList == null || !codeList.Any() || (codeList.Count == 1 && !codeList[0].Any()))
+                return 1;
+
+            var groupPosition = 0;
+            var itemPosition = 0;
+            for (var itemIndex = 0; itemIndex < shoppingCart.Count; itemIndex++)
+            {
+                var item = shoppingCart[itemIndex];
+                if (codeList[groupPosition][itemPosition] == item)
+                {
+                    itemPosition++;
+                }
+                else if (codeList[groupPosition][itemPosition] == "anything") 
+                {
+                    if (itemPosition == 0)
+                    {
+                        //we've matched this forever now
+                        codeList[groupPosition].RemoveAt(0);
+                    }
+                    else
+                    {
+                        itemPosition++;
+                    }
+                }
+                else
+                {
+                    //failure, reset
+                    itemPosition = 0;
+                }
+
+                if (itemPosition >= codeList[groupPosition].Count)
+                {
+                    groupPosition++;
+                    itemPosition = 0;
+                }
+
+                if (groupPosition >= codeList.Count)
+                {
+                    return 1;
+                }
+            }
+
+
+            return 0;
+        }
+    }
+}
---
+++ Prep.Tests/AWS/ShoppingCartTests.cs
@@ -0,0 +1,135 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prep.Problems.Problems.AWS;
+
+
+namespace Prep.Tests
+{
+    [TestClass]
+    public class ShoppingCartTests
+    {
+        private readonly ShoppingCart _solution = new ShoppingCart();
+        
+        [TestMethod]
+        public void Example1()
+        {
+
+            var result = _solution.Solve(
+                    new List<List<string>>() {
+                        new List<string>(){ "apple", "apple" },
+                        new List<string>(){ "banana", "anything", "banana" }
+                    },
+                    new List<string>() { "orange", "apple", "apple", "banana", "orange", "banana" }
+                );
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+
+            var result = _solution.Solve(
+                new List<List<string>>() {
+                    new List<string>(){ "apple", "apple" },
+                    new List<string>(){ "banana", "anything", "banana" }
+                },
+                new List<string>() { "banana", "orange", "banana", "apple", "apple" }
+            );
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+
+            var result = _solution.Solve(
+                new List<List<string>>() {
+                    new List<string>(){ "apple", "apple" },
+                    new List<string>(){ "banana", "anything", "banana" }
+                },
+                new List<string>() { "apple", "banana", "apple", "banana", "orange", "banana" }
+            );
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+
+            var result = _solution.Solve(
+                new List<List<string>>() {
+                    new List<string>(){ "apple", "apple" },
+                    new List<string>(){ "apple", "apple", "banana" }
+                },
+                new List<string>() { "apple", "apple", "apple", "banana" }
+            );
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Example5()
+        {
+
+            var result = _solution.Solve(
+                new List<List<string>>() {
+                    new List<string>(){ "apple", "apple" },
+                    new List<string>(){ "banana", "anything", "banana" }
+                },
+                new List<string>() { "orange", "apple", "apple", "banana", "orange", "banana" }
+            );
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Example6()
+        {
+
+            var result = _solution.Solve(
+                new List<List<string>>() {
+                    new List<string>(){ "apple", "apple" },
+                    new List<string>(){ "banana", "anything", "banana" }
+                },
+                new List<string>() { "apple", "apple", "orange", "orange", "banana", "apple", "banana", "banana" }
+            );
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Example7()
+        {
+
+            var result = _solution.Solve(
+                new List<List<string>>() {
+                    new List<string>(){ "anything", "apple" },
+                    new List<string>(){ "banana", "anything", "banana" }
+                },
+                new List<string>() { "orange", "grapes", "apple", "orange", "orange", "banana", "apple", "banana", "banana" }
+            );
+
+            Assert.AreEqual(1, result);
+        }
+
+
+        [TestMethod]
+        public void Example8()
+        {
+
+            var result = _solution.Solve(
+                new List<List<string>>() {
+                    new List<string>(){ "anything", "anything", "anything"},
+                    new List<string>(){ "anything", "anything", "banana" , "anything" }
+                },
+                new List<string>() { "orange", "grapes", "apple", "orange", "orange", "banana", "apple", "banana", "banana" }
+            );
+
+            Assert.AreEqual(1, result);
+        }
+
+    }
+}
